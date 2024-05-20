2bdbc88df354ed3a8953e9d698318b22a3cdf6ba
@0025755900 <hedieh.tahmouresi@email.kntu.ac.ir>
2024-05-20 13:15:31 +0330

    Modeling of the Bank!

---
+++ src/main/java/ir/ac/kntu/NeoBank.java
@@ -0,0 +1,10 @@
+package ir.ac.kntu;
+
+import java.util.List;
+
+public class NeoBank {
+    private List<SimpleUser> simpleUsers;
+    private List<Admin> admins;
+    private List<String> tracingNumbers;
+
+}
